eefc11997a1cc459d1d441bbc6791599b4ede7a3
jeison <jeison07777@gmail.com>
2022-05-25 11:18:19 -0500

    programando la logica de los textos registrarse y iniciar sección

--- app/src/main/java/com/example/barbersgold/MainActivity2.java
+++ app/src/main/java/com/example/barbersgold/MainActivity2.java
@@ -2,13 +2,31 @@ package com.example.barbersgold;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    TextView iniciar_seccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        iniciar_seccion = (TextView) findViewById(R.id.textIniciar);
+
+        iniciar_seccion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+
+
+        });
+
     }
 }
